0f22d300155973e86a77cb3c5ec27e0f04de7ae7
Héctor Fiorentino <hfiorentino@ifdevelopers.com.ar>
2014-08-10 17:18:24 -0300

    modificacion de service

--- js/index.js
+++ js/index.js
@@ -31,15 +31,16 @@ $(document).ready(function(){
 	//Buscar imagenes con dpi variable y reemplaza el folder por el adecuado.
 	$.post("http://banderablanca.org.ar/GetNews",{},function(exito){
 		if(exito){
-			$("loader").hide();
 			if(exito.newsType=="found"){
 				$("#foto").css("background-color","transparent");
 				$("#foto").attr("src","assets/imagenes/"+folder+"/banderaverde.png");
+				$("#desc").hide();
 			}else{
 				$("#foto").attr("src",exito.newsPictureURI);
+				$("#desc").html(exito.newsDescription);
 			}
-			$("#titulo").html(exito.newsTitle);
-			$("#desc").html(exito.newsDescription);
+			$("#titular").html(exito.newsTitle);
+			$("loader").hide();
 			$("#busqueda").show();
 		}
 	},"json")
